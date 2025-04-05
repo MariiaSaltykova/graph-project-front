21c842f6ead5e3dd4fca345594d9fe7826c3d6ed
saltykov <mariia.saltykova@here.com>
2025-04-05 18:28:26 +0200

    Revert "add project files"
    This reverts commit 1d973e368a59da250cfa278cbb7a4479c2bdbb56.

--- src/main/java/com/project/graphApp/model/Graph.java
+++
@@ -1,6 +0,0 @@
-package com.project.graphApp.graph.model;
-
-public interface Graph {
-    void generate(); // Metoda generująca strukturę grafu
-    String getInfo(); // Metoda zwracająca informacje o grafie
-}
